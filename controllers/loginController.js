29e29f09ff5c61ef4e4909d5fa5b01f261737192
borhanshuvo <borhan015@gmail.com>
2021-10-01 21:14:23 +0600

    changes

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -10,26 +10,32 @@ async function login(req, res, next) {
   try {
     // find a user who has this email/password
     const user = await User.findOne({ email: req.body.email });
-    const isValidPassword = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
+    if (user) {
+      const isValidPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
 
-    // user object
-    const userObject = {
-      id: user._id,
-      name: user.name,
-      email: user.email,
-    };
+      // user object
+      const userObject = {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      };
 
-    // token generate
-    const accessToken = jwt.sign(userObject, process.env.JWT_SECRET, {
-      expiresIn: "1d",
-    });
+      // token generate
+      const accessToken = jwt.sign(userObject, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+      });
 
-    if (isValidPassword) {
-      const { password, ...rest } = user._doc;
-      res.status(200).json({ user: rest, accessToken });
+      if (isValidPassword) {
+        const { password, ...rest } = user._doc;
+        res.status(200).json({ user: rest, accessToken });
+      } else {
+        res.status(500).json({
+          message: "Authentication error!",
+        });
+      }
     } else {
       res.status(500).json({
         message: "Authentication error!",
