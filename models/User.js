76c09a2aa53dae61ae75de7cbcf5328f6c5f13da
borhanshuvo <borhan015@gmail.com>
2021-10-12 16:05:47 +0600

    changes

--- models/User.js
+++ models/User.js
@@ -23,6 +23,10 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    category: {
+      type: String,
+      required: true,
+    },
     avatar: {
       type: String,
       default: "",
